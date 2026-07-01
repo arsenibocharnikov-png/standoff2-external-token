bfee921b97d63a3d41b1ade74f3f1c3b6f4aeb7e
arsenibocharnikov-png <arsenibocharnikov@gmail.com>
2026-07-02 00:20:04 +0300

    Update Android.mk to include C++ static libraries

--- Android.mk
+++ Android.mk
@@ -4,9 +4,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := token
 LOCAL_SRC_FILES := $(wildcard src/*.cpp) $(wildcard src/*/*.cpp)
+LOCAL_CPP_FEATURES := exceptions
 LOCAL_CFLAGS := -D_GNU_SOURCE
 LOCAL_CPPFLAGS := -std=c++17
-LOCAL_LDFLAGS := -static-libstdc++
-LOCAL_LDLIBS := -llog
+LOCAL_LDLIBS := -llog -lc++_static -latomic
 
 include $(BUILD_EXECUTABLE)
