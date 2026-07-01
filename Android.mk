49427d4c7d6273adca28a40ee4566b8dbf07821d
arsenibocharnikov-png <arsenibocharnikov@gmail.com>
2026-07-02 00:17:52 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := token
-LOCAL_SRC_FILES := src/main.cpp src/memory/memory.cpp src/process_manager/process_manager.cpp
+LOCAL_SRC_FILES := $(wildcard src/*.cpp) $(wildcard src/*/*.cpp)
 LOCAL_CFLAGS := -D_GNU_SOURCE
 LOCAL_CPPFLAGS := -std=c++17
 LOCAL_LDLIBS := -llog
