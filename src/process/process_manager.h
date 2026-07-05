4d76770625994fd19523636f0243deef19fe01a8
arsenibocharnikov-png <arsenibocharnikov@gmail.com>
2026-07-05 17:27:57 +0300

    Update process_manager.h

--- src/process/process_manager.h
+++ src/process/process_manager.h
@@ -8,11 +8,13 @@ public:
     bool initialize();
     int get_pid() const;
     uint64_t get_libunity_base() const;
+    uint64_t get_libil2cpp_base() const;
 
 private:
     std::string package_name_;
     int pid_;
     uint64_t libunity_base_;
+    uint64_t libil2cpp_base_;
 
     int find_process_id();
     uint64_t find_module_base(const char* module_name, int mod);
